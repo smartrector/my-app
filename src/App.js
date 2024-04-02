932063d766124d36039268cf3d062b8c64d41096
smartrector <smartrector@gmail.com>
2024-04-02 15:46:57 +0900

    map push

--- src/App.js
+++ src/App.js
@@ -4,41 +4,24 @@ import "./assets/css/style.css";
 function App() {
   const [num, setNum] = useState(0);
   const [data, setData] = useState([
-    "1.Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, nesciunt!",
-    "2.Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, nesciunt!",
-    "3.Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, nesciunt!",
+    "1.한Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, nesciunt!",
+    "2.성Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, nesciunt!",
+    "3.용Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, nesciunt!",
+    "4.용Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, nesciunt!",
   ]);
   return (
     <div>
-      <h2>tab menu를 제작하세요</h2>
-      {/* {data[0]} */}
-
       <ul className="menu">
-        {/* {data.map((item, idx) => {
+        {data.map((han, idx) => {
           return (
             <>
               <li
-                className={`${idx == num ? "active" : ""}`}
+                className={`tjklte ${idx == num ? "active" : ""}`}
                 onClick={() => {
                   setNum(idx);
                 }}
               >
                 tab{idx + 1}
-              </li>
-            </>
-          );
-        })} */}
-
-        {data.map((item, idx) => {
-          return (
-            <>
-              <li
-                className={`test ${idx == num ? "active" : ""}`}
-                onClick={() => {
-                  setNum(idx);
-                }}
-              >
-                tab{idx}
               </li>
             </>
           );
